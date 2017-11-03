ff85fabfb742db36617a02b1654c38ef03ef5fe3
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-11-03 15:53:28 +0530

    recycler view practice

---
+++ Application2(RecyclerView)/app/src/main/java/RecyclerAdapter.java
@@ -0,0 +1,13 @@
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by AlphaBAT69 on 03-11-2017.
+ */
+class RecyclerHolder extends RecyclerView.ViewHolder{
+    public RecyclerHolder(View itemView){
+        super(itemView);
+    }
+}
+public class RecyclerAdapter {
+}
