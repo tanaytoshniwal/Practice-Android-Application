bd00f4e67fef27e449c3bb79126809413a151ceb
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2018-02-04 05:13:39 +0530

    notifications application demo

---
+++ Notification Demo/app/src/main/java/com/example/alphabat69/notificationsdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.alphabat69.notificationsdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
